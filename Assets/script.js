f613ba7c973a283df71aac3930bebcc4d4237f59
Bro74 <bmwhite1014@gmail.com>
2023-05-07 17:20:06 -0400

    added global variables and search btns

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,35 @@
 var APIKey= "43b6171e5206fe7a36d0cfb460f4f324";
 
+//GLobal varaible list 
+var cityName= document.querySelector("#search-result-name");
+var temp = document.querySelector("#temp");
+var humidity= document.querySelector("#humidity");
+var wind = document.querySelector("#wind");
+var forecastCityName= document.querySelector("#forecast-city-name");
+var dayOneDate= document.querySelector("#dayOneDate");
+var dayTwoDate=document.querySelector("#dayTwoDate");
+var dayThreeDate=document.querySelector("#dayThreeDate");
+var dayFourDate= document.querySelector("#dayFourDate");
+var dayFiveDate= document.querySelector("#dayFiveDate");
+var dayOneTemp= document.querySelector("#dayOneTemp");
+var dayTwoTemp= document.querySelector("#dayTwoTemp");
+var dayThreeTemp= document.querySelector("#dayThreeTemp");
+var dayFourTemp= document.querySelector("#dayFourTemp");
+var dayFiveTemp= document.querySelector("#dayFiveTemp");
+var dayOneHumidity= document.querySelector("#dayOneHumidity");
+var dayTwoHumidity=document.querySelector("#dayTwoHumidity");
+var dayThreeHumidity=document.querySelector("#dayThreeHumidity");
+var dayFourHumidity= document.querySelector("#dayFourHumidity");
+var dayFiveHumidity= document.querySelector("#dayFiveHumidity");
+var dayOneWind= document.querySelector("#dayOneWind");
+var dayTwoWind=document.querySelector("#dayTwoWind");
+var dayThreeWind=document.querySelector("#dayThreeWind");
+var dayFourWind=document.querySelector("#dayFourWind");
+var dayFiveWind=document.querySelector("#dayFiveWind");
+
+var buttonList= document.querySelector(".savedButtons");
+
+//Variable for search button element
+var searchButton = document.querySelector(".btn");
+//Variable for search name input
+var searchName = document.querySelector("#inputCityName2");
